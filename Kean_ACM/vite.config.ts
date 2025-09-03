69819c87858a1eba6b4f6454914a7853d830c8ae
Karangarha <garhasaab001@gmail.com>
2025-09-03 14:44:54 -0400

    initial commit

--- Kean_ACM/vite.config.ts
+++ Kean_ACM/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Kean_ACM",
+  base: "/Kean_ACM/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
